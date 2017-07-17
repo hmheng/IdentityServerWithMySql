3b26247e55414cbb4ce8ef0bd070ecd4af74e54e
Marvin Heng <Marvin.Heng@avire-global.com>
2017-07-17 15:33:33 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IdentityServerWithMySql.Models;
+
+namespace IdentityServerWithMySql.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.SetCommandTimeout(120);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Quickstart/Account/LogoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServerWithMySql.Quickstart.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerWithMySql.Quickstart.UI
+{
+    public class LogoutViewModel : LogoutInputModel
+    {
+        public bool ShowLogoutPrompt { get; set; }
+    }
+}
